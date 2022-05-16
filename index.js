48fff1d9b8d7bd98ec48279b50d22e6f995e367e
noormohammad20 <mdsaim204@gmail.com>
2022-05-17 01:53:47 +0600

    Save Doctor info in the database and display success message

--- index.js
+++ index.js
@@ -47,10 +47,11 @@ async function run() {
         const serviceCollection = client.db('doctors_portal').collection('services')
         const bookingCollection = client.db('doctors_portal').collection('booking')
         const userCollection = client.db('doctors_portal').collection('users')
+        const doctorCollection = client.db('doctors_portal').collection('doctors')
 
         app.get('/service', async (req, res) => {
             const query = {}
-            const cursor = serviceCollection.find(query)
+            const cursor = serviceCollection.find(query).project({ name: 1 })
             const services = await cursor.toArray()
             res.send(services)
         })
@@ -160,6 +161,12 @@ async function run() {
             }
             const result = await bookingCollection.insertOne(booking)
             return res.send({ success: true, result })
+        })
+
+        app.post('/doctor', async (req, res) => {
+            const doctor = req.body
+            const result = await doctorCollection.insertOne(doctor)
+            res.send(result)
         })
     }
     finally {
