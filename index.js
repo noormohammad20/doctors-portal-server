d449df0a2ac26bda47123444042156199dd46b20
noormohammad20 <mdsaim204@gmail.com>
2022-05-16 22:53:31 +0600

    some added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.options("*", cors(corsConfig))
 app.use(express.json())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
     next()
 })
 
