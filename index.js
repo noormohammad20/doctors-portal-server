0c90e0dfe8e6949c2a0f18c7612864e7060c1fb9
noormohammad20 <mdsaim204@gmail.com>
2022-05-18 01:40:33 +0600

    Server side payment intents API to get client Secret from stripe

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const jwt = require('jsonwebtoken')
 var nodemailer = require('nodemailer')
 const Sib = require('sib-api-v3-sdk')
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -99,6 +99,18 @@ async function run() {
                 res.status(403).send({ message: 'Forbidden Access' })
             }
         }
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const service = req.body
+            const price = service.price
+            const amount = price * 100
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+            res.send({ clientSecret: paymentIntent.client_secret })
+        })
 
         app.get('/service', async (req, res) => {
             const query = {}
